2f44e879f01bcabcef735490c2923c1dd1606bb6
Ndewedo Newbury <nfnewbury@dons.usfca.edu>
2025-09-14 13:37:08 -0700

    fix gitignore

---
+++ Gamers Cove/src/main/java/org/example/gamerscove/GamersCoveApplication.java
@@ -0,0 +1,13 @@
+package org.example.gamerscove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersCoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamersCoveApplication.class, args);
+    }
+
+}
---
+++ Gamers Cove/src/test/java/org/example/gamerscove/GamersCoveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gamerscove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersCoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
