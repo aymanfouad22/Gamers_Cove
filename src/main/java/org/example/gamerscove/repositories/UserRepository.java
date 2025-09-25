df6949202c6fb27d07accd5c21c74df7e2d17681
Ndewedo Newbury <nfnewbury@dons.usfca.edu>
2025-09-24 21:52:03 -0700

    began api implementation on user, de-coupled data accessing logic.

--- src/main/java/org/example/gamerscove/repositories/UserRepository.java
+++ src/main/java/org/example/gamerscove/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package org.example.gamerscove.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.example.gamerscove.domain.entities.UserEntity;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity,Integer> {
 }
